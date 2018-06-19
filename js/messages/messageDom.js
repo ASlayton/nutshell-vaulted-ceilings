7f5343a4dd0df6e36b80a49b703cf30f4e500943
Anna Dolan <anna.dolan115@gmail.com>
2018-06-18 21:23:45 -0500

    wip user specific

--- js/messages/messageDom.js
+++ js/messages/messageDom.js
@@ -1,21 +1,4 @@
-const {getAllMessages, getAllUsers,} = require('./messageFirebase');
-
-getAllMessages().then((results) => {
-  const messages = results;
-  console.log(messages);
-  messages.forEach((message) => {
-    getAllUsers().then((results) => {
-      const users = results;
-      console.log(users);
-      users.forEach((user) => {
-        if (user.uid === message.uid) {
-          .push(user.uid);
-        }
-      });
-    });
-  });
-});
-
+// const {getAllUsers,} = require('./messageFirebase');
 const printAllMessagesToDom = (strangs) => {
   $('#message-container').html(strangs);
 };
