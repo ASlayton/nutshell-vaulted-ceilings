001d13eed998972054dc5c2e124ff9ef8a1866ea
Barry Russell <blrussell09@gmail.com>
2018-06-19 09:15:59 -0500

    changed my mind, threw an if statement in to display friends events without the buttons

--- js/events/eventsDom.js
+++ js/events/eventsDom.js
@@ -3,16 +3,20 @@ const eventDomString = (eventArray, whereAt) => {
   strang += `<div class="container-fluid">`;
   strang +=   `<div class="row text-center">`;
   eventArray.forEach((event) => {
+    console.log(event);
     strang +=   `<div class="col-md-3 event-card" data-firebase-id="${event.id}">`;
     strang +=     `<h3 class="evt-title">${event.title}</h3>`;
     strang +=     `<ul class="list-group">`;
     strang +=       `<li class="list-group-item">Date: <span class="evt-date">${event.date}</li>`;
     strang +=       `<li class="list-group-item">Location: <span class="evt-loc">${event.location}</li>`;
     strang +=     `</ul>`;
-    strang +=   `<div class="btn-group" role="group">`;
-    strang +=     `<button type="button" class="deleteEvent btn btn-primary btn-sm"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span> Delete</button>`;
-    strang +=     `<button type="button" class="editUserEvent btn btn-danger btn-sm" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span> Edit</button>`;
-    strang +=   `</div>`;
+    if (event.uid === firebase.auth().currentUser.uid)
+    {
+      strang +=   `<div class="btn-group" role="group">`;
+      strang +=     `<button type="button" class="deleteEvent btn btn-primary btn-sm"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span> Delete</button>`;
+      strang +=     `<button type="button" class="editUserEvent btn btn-danger btn-sm" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span> Edit</button>`;
+      strang +=   `</div>`;
+    }
     strang +=  `</div>`;
   });
   strang +=   `</div>`;
